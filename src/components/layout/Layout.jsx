63316d829ecac6d5af37a91abed5e49f2208c296
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-01-01 05:22:46 +0530

    Add motion

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import { motion, useAnimation, useInView } from "motion/react";
 
 const Layout = ({ children }) => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, {once: true})
+  const mainControls = useAnimation()
+
+  useEffect(() => {
+    if(isInView){
+      mainControls.start("visible")
+    }
+  })
   return (
-    <div>
+    <div ref={ref}>
       <Navbar />
-      <div>{children}</div>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{duration: 0.5, delay: 0.25}}
+      >
+        {children}
+      </motion.div>
 
       <Footer />
     </div>
